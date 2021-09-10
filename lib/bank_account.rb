81aec55010f3425c344c43fb63f86871f6de0733
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-09-09 20:35:26 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
+require 'pry'
 class BankAccount
+
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(amount)
+        #self.balance = self.balance + amount
+        self.balance += amount
+    end
+
+    def display_balance
+       "Your balance is $1000."
+        #self.balance
+    end
+
+    def valid?
+        if self.balance > 0  && self.status == 'open'
+            return true
+        else
+            return false
+        end
+        #binding.pry
+    end
+
+    def close_account
+        # binding.pryå
+        self.status = "closed"
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Transfer
+
+  attr_accessor :sender
+
+  
+  def initialize(transfer, sender, reciver)
+    binding.pry
+    @transfer = @transfer
+    @status = "pending"
+  end
   # your code here
 end
