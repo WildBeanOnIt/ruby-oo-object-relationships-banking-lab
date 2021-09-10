ce6dab037a36aeacdab615fee5609b208c40a116
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-09-09 21:41:27 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,15 +1,28 @@
 require 'pry'
 class Transfer
 
-  attr_reader :status
+  attr_reader :status, :sender, :receiver, :amount
 
-  
-  def initialize(transfer, sender, receiver)
+  def initialize(sender, receiver, amount)
     #binding.pry
-    @receiver = @receiver
-    @sender = @sender
-    @transfer = @transfer
+    @receiver = receiver
+    @sender = sender
+    @amount = amount
     @status = "pending"
+  end
+
+  def valid?
+    #binding.pry
+    if sender.status == 'false' && receiver.status == 'false'
+      return true
+    else
+      return false
+    end
+    
+    sender.valid? && receiver.valid?
+
+    transfer.valid?
+
   end
   # your code here
 end
